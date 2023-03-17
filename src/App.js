6eb3528145aca927f6bb77609676b5a772b26d24
Griffin <116022056+GriffinSwain@users.noreply.github.com>
2023-03-17 15:15:16 -0700

    Added Peer Review

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@
 // This React Application is designed to look like a design document that we were given.
 // It is designed to be responsivess on screen sizes '1440' and '375'
 
+/*
+Peer Review By Brandon Le:  
+Really good job overall.  A few things I would fix is the top header bar looks like it comes down 
+too far past that first row of cards. The toggle for dark and light mode worked really well!  
+If my eyes are okay I think the text  shifts a little bit when you change from dark and light mode, but nothing too noticeable. 
+I also noticed a lot of bottom margin on the mobile version but really good getting all the important functions working! 
+I wasn't able to get the dark and light modes working so it was nice to look into the code and see how Griffin was able to get it done.
+*/
+
+
 import React, { useEffect, useState } from 'react';
 import Desktop from './Components/desktop';
 import Mobile from './Components/mobile';
